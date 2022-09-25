7774f3b1406f6495e403c2c49c272668ab07b9c2
Varun Sharma <varun_sharma@live.co.uk>
2022-09-24 21:41:09 -0700

    feat: update hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,4 +3,10 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    goerli: {
+      url: process.env.QUICKNODE_API_KEY_URL,
+      accounts: [process.env.GOERLI_PRIVATE_KEY],
+    },
+  },
 };
